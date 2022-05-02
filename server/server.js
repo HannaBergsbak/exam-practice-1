8d6ddafe4e58e1496c456da479fb41fb2681480b
Hanna Bergsbak <hannabergsbak@hotmail.com>
2022-05-02 13:34:07 +0200

    help

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import * as path from "path";
+import {MoviesApi} from "./MoviesApi.js";
 
 const app = express();
+
+app.use("/api/movies", MoviesApi());
 
 app.use(express.static("../client/dist/"));
 
 app.use((req, res, next) => {
-    if (req.method == "GET" && !req.path.startsWith("/api")){
-        return res.sendFile(path.resolve("../client/dist/index.html"))
+    if (req.method === "GET" && !req.path.startsWith("/api")){
+        res.sendFile(path.resolve("../client/dist/index.html"));
     } else {
         next();
     }
-
 });
 
 
